7f5811283dd4347fd1879c3847c85edc9546eca7
Fulati Paerhati <john.parhat@gmail.com>
2024-02-11 08:05:16 -0600

    complete update listing functionalities

--- api/routes/listing.rout.js
+++ api/routes/listing.rout.js
@@ -3,12 +3,14 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 import { verify } from 'crypto'
 
 const router = express.Router()
 
+router.get('/get/:id', getListing)
 router.post('/create', verifyToken, createListing)
 router.delete(`/delete/:id`, verifyToken, deleteListing)
 router.put(`/update/:id`, verifyToken, updateListing)
